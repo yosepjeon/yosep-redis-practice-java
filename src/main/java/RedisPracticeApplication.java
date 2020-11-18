e552e2760a31c82568db5d32fd1b2a9ae0371afb
요깨비 <yoggaebi@yokkaebiui-MacBookPro.local>
2020-11-18 23:28:38 +0900

    redis 연습 시작

---
+++ src/main/java/RedisPracticeApplication.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPracticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisPracticeApplication.class, args);
+    }
+}
