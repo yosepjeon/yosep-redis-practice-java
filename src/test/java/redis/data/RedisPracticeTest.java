3fc2d832ed020cdba8a776cadb8436557a7ad377
요깨비 <yoggaebi@yokkaebiui-MacBookPro.local>
2020-11-20 00:47:54 +0900

    list, hash 학습완료

--- src/test/java/redis/data/RedisPracticeTest.java
+++ src/test/java/redis/data/RedisPracticeTest.java
@@ -4,20 +4,23 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.connection.DataType;
 import org.springframework.data.redis.connection.RedisServer;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 //import yosep.com.redis.config.EmbeddedRedisConfig;
 import yosep.com.redis.config.RedisRepositoryConfig;
 
-import java.util.Arrays;
-import java.util.Date;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.Matchers.greaterThan;
@@ -26,6 +29,8 @@ import static org.hamcrest.Matchers.greaterThan;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = RedisPracticeTest.class)
 public class RedisPracticeTest {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private RedisTemplate<String ,String> redisTemplate;
 
@@ -64,5 +69,73 @@ public class RedisPracticeTest {
 
         // Key 일괄 삭제
         Assert.assertThat(redisTemplate.delete(Arrays.asList("key1","key2","key3")), greaterThan(0L));
+    }
+
+    /*
+    * 문자 데이터 구조 처리
+     */
+    @Test
+    public void opsValue() {
+        ValueOperations<String, String> valueOps = redisTemplate.opsForValue();
+        Collection<String> cacheKeys = new ArrayList<>();
+        String cacheKey = "value_";
+
+        for(int i=0;i<10;i++) {
+            cacheKeys.add(cacheKey + i);
+            valueOps.set(cacheKey + i, String.valueOf(i),60, TimeUnit.SECONDS);
+        }
+
+        List<String> values = valueOps.multiGet(cacheKeys);
+        Assert.assertNotNull(values);
+        Assert.assertEquals(10, values.size());
+        log.info("##### opsValue #####");
+        log.info("{}", values);
+    }
+
+    /*
+    * List 데이터 구조 처리 - 순서 있음. value 중복 허용
+     */
+    @Test
+    public void opsList() {
+        ListOperations<String, String> listOps = redisTemplate.opsForList();
+        String cacheKey = "valueList";
+
+        for(int i=0;i<10;i++) {
+            listOps.leftPush(cacheKey,String.valueOf(i));
+        }
+
+        Assert.assertSame(DataType.LIST, redisTemplate.type(cacheKey));
+        Assert.assertSame(10L, listOps.size(cacheKey));
+        log.info("##### opsList #####");
+        log.info("{}", listOps.range(cacheKey,0,10));
+        Assert.assertEquals("0", listOps.rightPop(cacheKey));
+        Assert.assertEquals("9", listOps.leftPop(cacheKey));
+        Assert.assertEquals(true, redisTemplate.delete(cacheKey));
+        log.info("{}", listOps.range(cacheKey,0,10));
+    }
+
+    /*
+    * Hash 데이터 구조 처리 - 순서 없음. key 중복허용 안함, value 중복 허용
+     */
+    @Test
+    public void opsHash() {
+        HashOperations<String, String, String> hashOps = redisTemplate.opsForHash();
+        String cacheKey = "valueHash";
+
+        for(int i = 0; i< 10;i++) {
+            hashOps.put(cacheKey, "key_" + i,"value_" + i);
+        }
+
+        Assert.assertSame(DataType.HASH, redisTemplate.type(cacheKey));
+        Assert.assertSame(10L, hashOps.size(cacheKey));
+        log.info("##### opsHash #####");
+        Set<String> hKeys = hashOps.keys(cacheKey);
+        for(String hKey : hKeys) {
+            log.info("{} / {}", hKey, hashOps.get(cacheKey, hKey));
+        }
+
+        Assert.assertEquals("value_5", hashOps.get(cacheKey, "key_5"));
+        Assert.assertEquals(Optional.of(1L).get(), hashOps.delete(cacheKey, "key_5"));
+        Assert.assertEquals(null, hashOps.get(cacheKey,"key_5"));
     }
 }
