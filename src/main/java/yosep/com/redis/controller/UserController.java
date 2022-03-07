c5d886b3cd19005ae6dc8be54803d5d505ec3b13
yosepjeon <enekelx1@naver.com>
2022-03-07 23:52:41 +0900

    jpa 기본기능 구현

--- src/main/java/yosep/com/redis/controller/UserController.java
+++ src/main/java/yosep/com/redis/controller/UserController.java
@@ -7,15 +7,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import yosep.com.redis.common.config.cachekey.UserCacheKey;
+import yosep.com.redis.data.dto.UserDto;
+import yosep.com.redis.service.UserService;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @Cacheable(value = UserCacheKey.USER, key = "#userId", unless = "#result == null")
     @GetMapping("/{id}")
     public ResponseEntity findUserByUserId(@PathVariable String userId) {
+        UserDto userDto = userService.findUserByUserId(userId);
 
-
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(userDto);
     }
 }
